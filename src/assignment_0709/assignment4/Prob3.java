227653227ab13d94016ffb115fd5359be6922a42
LeeJiEun <gsilverg197@gmail.com>
2024-07-10 13:56:04 +0900

    [Feat] Add 0709 String3 assignment4_3

---
+++ src/assignment_0709/assignment4/Prob3.java
@@ -0,0 +1,17 @@
+package assignment_0709.assignment4;
+
+public class Prob3 {
+    public static void main(String[] args) {
+        Prob3 p = new Prob3();
+        String addr = "서울시,강남구,봉은사로,SSGJavaBackend";
+        String[] addrArr = p.split(addr, ',');
+        System.out.println("배열 크기 : " + addrArr.length);
+        for(int i=0; i<addrArr.length; i++){
+            System.out.print(addrArr[i] + " ");
+        }
+    }
+
+    public String[] split(String str, char separator){
+        return str.split(String.valueOf(separator));
+    }
+}
