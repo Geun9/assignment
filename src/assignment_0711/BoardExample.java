037cc818fc8e14acae4c1d62be6acc7a343cbe5b
LeeJiEun <gsilverg197@gmail.com>
2024-07-14 21:57:05 +0900

    Feat: Add functionality to update selected board

--- src/assignment_0711/BoardExample.java
+++ src/assignment_0711/BoardExample.java
@@ -13,6 +13,7 @@ public class BoardExample {
 
     private static final String MENU_NUMBER = "[1-4]+";
     private static final String SUB_MENU_NUMBER = "[1-2]+";
+    private static final String READ_SUB_MENU_NUMBER = "[1-3]+";
     private static boolean isQuit = false;
     private static List<Board> boardList = new ArrayList<>();
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -50,10 +51,10 @@ public class BoardExample {
         String inputMenuSelect = br.readLine();
         if (inputMenuSelect.matches(MENU_NUMBER)) {
             switch (Integer.parseInt(inputMenuSelect)) {
-                case 1 -> createBoard();
-                case 2 -> readBoard();
-                case 3 -> clearBoard();
-                case 4 -> exitProgram();
+                case 1 -> create();
+                case 2 -> read();
+                case 3 -> clear();
+                case 4 -> exit();
             }
         } else {
             System.out.println("\n없는 메뉴 번호입니다.\n다시 입력해주세요.\n");
@@ -63,7 +64,7 @@ public class BoardExample {
     /*
      * 게시글 생성
      */
-    private static void createBoard() throws IOException {
+    private static void create() throws IOException {
         System.out.println(BoardText.CREATE_BOARD.getText());
         System.out.print(BoardText.TITLE.getText());
         String title = br.readLine();
@@ -87,7 +88,7 @@ public class BoardExample {
     /*
      * 게시글 읽기
      */
-    private static void readBoard() throws IOException{
+    private static void read() throws IOException{
         System.out.println(BoardText.READ_BOARD.getText());
         System.out.print(BoardText.BNO.getText());
 
@@ -101,17 +102,64 @@ public class BoardExample {
                 System.out.println(BoardText.WRITER.getText() + board.getBwriter());
                 System.out.println(BoardText.DATE.getText() + board.getDate());
                 System.out.println(BoardText.BOARD_SEPARATOR.getText());
-
+                readSubMenu(board);
                 break;
             }
         }
     }
 
-    private static void clearBoard() {
+    /*
+     * 게시글 읽기 > 수정/삭제/목록 보기
+     */
+    private static void readSubMenu(Board board) throws IOException {
+        System.out.println(BoardText.BORDER_LINE.getText());
+        System.out.println(BoardText.READ_SUB_MENU.getText());
+        System.out.print(BoardText.MENU_SELECT.getText());
+
+        String inputSubMenu = br.readLine();
+        if (inputSubMenu.matches(READ_SUB_MENU_NUMBER)) {
+            switch (Integer.parseInt(inputSubMenu)) {
+                case 1 -> update(board);
+                case 2 -> delete(board);
+            }
+        }
+    }
+
+    /*
+     * 게시글 수정
+     */
+    private static void update(Board board) throws IOException{
+        System.out.println(BoardText.UPDATE_BOARD.getText());
+        System.out.print(BoardText.TITLE.getText());
+        String title = br.readLine();
+        System.out.print(BoardText.CONTENT.getText());
+        String content = br.readLine();
+        System.out.print(BoardText.WRITER.getText());
+        String writer = br.readLine();
+
+        /*
+         * 게시글 수정 확인
+         */
+        printSubMenu();
+        String inputSubMenu = br.readLine();
+        if (inputSubMenu.matches(SUB_MENU_NUMBER)) {
+            if (Integer.parseInt(inputSubMenu) == 1) {
+                board.setBtitle(title);
+                board.setBcontent(content);
+                board.setBwriter(writer);
+            }
+        }
+    }
+
+    private static void delete(Board board) {
 
     }
 
-    private static void exitProgram() {
+    private static void clear() {
+
+    }
+
+    private static void exit() {
         isQuit = !isQuit;
     }
 
@@ -119,11 +167,5 @@ public class BoardExample {
         System.out.println(BoardText.BORDER_LINE.getText());
         System.out.println(BoardText.SUB_MENU.getText());
         System.out.print(BoardText.MENU_SELECT.getText());
-    }
-
-    private static String getDate() {
-        LocalDate now = LocalDate.now();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd");
-        return simpleDateFormat.format(now);
     }
 }
